92fd8409a47a2935ef9ca8bed16fcf3023dfaa07
taeseong <tshong3@naver.com>
2026-05-03 05:35:14 +0000

    feat: 센서 API 추가

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,14 @@ const cors = require("cors");
 require("dotenv").config();
 
 const authRoutes = require("./routes/auth");
-
+const sensorRoutes = require("./routes/sensor");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/sensor-data", sensorRoutes);
 
 app.get("/api", (req, res) => {
   res.json({ message: "backend working" });
